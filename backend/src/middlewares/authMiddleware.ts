d40d7d0e739781cdf07468b545e8c686d4ee7359
Lucas Steinbach <lucaswsb52@gmail.com>
2026-08-06 19:12:14 -0300

    corrigindo mensagem de autenticação

--- backend/src/middlewares/authMiddleware.ts
+++ backend/src/middlewares/authMiddleware.ts
@@ -10,7 +10,7 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
     const authHeader = req.headers.authorization;
 
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Token não informado" });
+        return res.status(401).json({ message: "Usuário não autenticado." });
     }
 
     const token = authHeader.split(" ")[1];
